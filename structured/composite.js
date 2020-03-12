ffaf07340abb2ce826e105f5702ac955f839f93c
Alexandr Kudryashov <kudryashov_as@astral.ru>
2020-03-12 12:21:27 +0300

    8

---
+++ structured/composite.js
@@ -0,0 +1,71 @@
+// позволяет скомпоновать множество объекьлв в древовидную структуру, и работать так буд-то бы это один единственный объект
+
+class Equipment {
+  getPrice() {
+    return this.price || 0;
+  }
+  getName() {
+    return this.name;
+  }
+  setName(name) {
+    this.name = name;
+  }
+  setPrice(price) {
+    this.price = price;
+  }
+}
+
+class Engine extends Equipment {
+  constructor() {
+    super();
+    this.setPrice("Engine");
+    this.setPrice(800);
+  }
+}
+
+class Body extends Equipment {
+  constructor() {
+    super();
+    this.setName("Body");
+    this.setPrice(3000);
+  }
+}
+
+class Tools extends Equipment {
+  constructor() {
+    super();
+    this.setNam("Tools");
+    this.setPrice(4000);
+  }
+}
+
+class Composite extends Equipment {
+  constructor() {
+    super();
+    this.equipments = [];
+  }
+  add(equipment) {
+    this.equipments.push(equipment);
+  }
+  getPrice() {
+    return this.equipments
+      .map(equipment => {
+        equipment.getPrice();
+      })
+      .reduce((a, b) => a + b);
+  }
+}
+
+class Car extends Composite {
+  constructor() {
+    super();
+    this.setName("Audi");
+  }
+}
+
+const myCar = new Car();
+
+myCar.add(new Engine());
+myCar.add(new Body());
+
+console.log(myCar.getPrice());
